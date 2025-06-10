1a8f5eb82667895d1f3c554df16588e39f01bb52
Cryptoking94 <98610415+Cryptoking94@users.noreply.github.com>
2025-06-10 10:58:09 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,3 @@
+output "web_instance_id" { value = module.web.web_instance_id }
+output "app_instance_id" { value = module.app.app_instance_id }
+output "db_endpoint" { value = module.db.db_endpoint }
