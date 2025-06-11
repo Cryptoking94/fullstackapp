0d8ad7a6fa1fc0ba5e1e76bb4f2eb29524cd0165
Cryptoking94 <98610415+Cryptoking94@users.noreply.github.com>
2025-06-11 10:16:44 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -12,7 +12,7 @@ resource "aws_subnet" "public" {
 }
 
 resource "aws_subnet" "private" {
-  count = length(var.private_subnets)
+  coun = length(var.private_subnets)
   vpc_id = aws_vpc.this.id
   cidr_block = var.private_subnets[count.index]
   availability_zone = element(data.aws_availability_zones.available.names, count.index)
